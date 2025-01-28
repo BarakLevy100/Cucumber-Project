a5e19815f6e42d01e42d10f6d8c0dba89aea7d6d
barak <barakilevy@edu.hac.ac.il>
2025-01-28 12:50:18 +0200

    The Cucumber test project.

---
+++ features/step_definitions/first_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I click on Got it button$/) do
+  puts("clicked Got it button")
+end
+
+And(/^I see Sq kilometre in From header$/) do
+  puts("Saw Sq Km in From header")
+end
+
+And(/^I see Sq Metre in to header$/) do
+  puts("Saw Sq Mtr in To header")
+end
